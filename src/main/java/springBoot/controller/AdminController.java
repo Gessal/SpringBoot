a25f3b3e40b5fb590c9119e5365e77ea3a794616
Gessal <gessalmaxim@gmail.com>
2020-05-27 22:10:44 +0300

    done

--- src/main/java/springBoot/controller/AdminController.java
+++ src/main/java/springBoot/controller/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,53 +32,25 @@ public class AdminController {
         return "admin";
     }
 
-    @PostMapping("/admin/users")
-    public String deleteUser(@RequestParam(name = "id") Long id, ModelMap model) {
-        User user = service.get(id);
+    @PostMapping("/admin/delete")
+    public String deleteUser(@RequestParam(name = "id") Long id) {
         service.delete(id);
-        List<User> users = service.list();
-        model.addAttribute("users", users);
-        return "_users";
-    }
-
-    @GetMapping("/admin/update")
-    public String printUserForUpdate(@RequestParam(name = "id") Long id, ModelMap model) {
-        User user = service.get(id);
-        boolean isUser = false;
-        boolean isAdmin = false;
-        for (Role role : user.getRoles()) {
-            if (role.getRole().equals("ROLE_USER")) {
-                isUser = true;
-            } else if (role.getRole().equals("ROLE_ADMIN")) {
-                isAdmin = true;
-            }
-        }
-        model.addAttribute("user", user);
-        model.addAttribute("isUser", isUser);
-        model.addAttribute("isAdmin", isAdmin);
-        return "update";
+        return "redirect:/admin";
     }
 
     @PostMapping("/admin/update")
     public String updateUser(@RequestParam(name = "id") Long id, @RequestParam(name = "username") String username,
                              @RequestParam(name = "password") String password, @RequestParam(name = "name") String name,
                              @RequestParam(name = "surname") String surname, @RequestParam(name = "age") byte age,
-                             @RequestParam(name = "isEnabled", required = false) boolean isEnabled,
-                             @RequestParam(name = "isUser", required = false) boolean isUser,
-                             @RequestParam(name = "isAdmin", required = false) boolean isAdmin, ModelMap model) {
-        User user = new User(id, username, password, name, surname, age, isEnabled ? (byte) 1 : 0);
-        List<Role> roles = new ArrayList<>();
-        if (isUser) {
-            roles.add(new Role("ROLE_USER"));
+                             @RequestParam(name = "roles", required = false) String[] roles) {
+        User user = new User(id, username, password, name, surname, age, (byte) 1);
+        List<Role> roleList = new ArrayList<>();
+        for (String r : roles) {
+            roleList.add(new Role(r));
         }
-        if (isAdmin) {
-            roles.add(new Role("ROLE_ADMIN"));
-        }
-        user.setRoles(roles);
+        user.setRoles(roleList);
         service.set(user);
-        List<User> users = service.list();
-        model.addAttribute("users", users);
-        return "admin";
+        return "redirect:/admin";
     }
 
     @PostMapping("/admin")
